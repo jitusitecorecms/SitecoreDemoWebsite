13128e3466ad3cb2cb9593f7cc6938eec2a34257
My Sitecore Diaries <80027240+jitusitecorecms@users.noreply.github.com>
2025-02-20 18:30:06 +0530

    Update

---
+++ DemoSCWebsite/MSD.DemoWebsite/Custom Fields/Captcha/CaptchaV3Validator.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json.Linq;
+using Sitecore.Data;
+using Sitecore.ExperienceForms.Mvc.Models.Fields;
+using Sitecore.ExperienceForms.Mvc.Models.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace MSD.DemoWebsite.Custom_Fields.Captcha
+{
+    public class CaptchaV3Validator : ValidationElement<string>
+    {
+        public override IEnumerable<ModelClientValidationRule> ClientValidationRules
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(this.ApiKey))
+                {
+                    yield break;
+                }
+            }
+        }
+
+        protected virtual string ApiKey { get; set; }
+        protected virtual string Title { get; set; }
+        protected virtual string FieldName { get; set; }
+
+        public CaptchaV3Validator(ValidationDataModel validationItem) : base(validationItem)
+        {
+
+        }
+
+        public override void Initialize(object validationModel)
+        {
+            base.Initialize(validationModel);
+            StringInputViewModel stringInputViewModel = validationModel as StringInputViewModel;
+            if (stringInputViewModel != null)
+            {
+                var fieldItem = Sitecore.Context.Database.GetItem(ID.Parse(stringInputViewModel.ItemId));
+                if (fieldItem != null)
+                {
+                    this.ApiKey = fieldItem["Api Key"];
+                }
+                this.Title = stringInputViewModel.Title;
+                this.FieldName = stringInputViewModel.Name;
+            }
+        }
+
+        public override ValidationResult Validate(object value)
+        {
+            if (value == null)
+            {
+                return new ValidationResult("Captcha is Inavlid.");// ValidationResult.Success;
+            }
+            var isCaptchaValid = ValidateCaptcha((string)value, this.ApiKey);
+            if (!isCaptchaValid)
+            {
+                return new ValidationResult(this.FormatMessage(new object[] { this.Title }));
+            }
+            return ValidationResult.Success;
+        }
+        public static bool ValidateCaptcha(string response, string secret)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            var res = httpClient.GetAsync($"https://www.google.com/recaptcha/api/siteverify?secret=" + secret + "&response=" + response + "").Result;
+
+            if (res.StatusCode != HttpStatusCode.OK)
+                return false;
+
+            string JSONres = res.Content.ReadAsStringAsync().Result;
+            dynamic JSONdata = JObject.Parse(JSONres);
+
+            if (JSONdata.success != "true")
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ DemoSCWebsite/MSD.DemoWebsite/Custom Fields/Captcha/GoogleRecaptchaV3.cs
@@ -0,0 +1,35 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.ExperienceForms.Mvc.Models.Fields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSD.DemoWebsite.Custom_Fields.Captcha
+{
+    public class GoogleRecaptchaV3 : StringInputViewModel
+    {
+        public string ApiKey { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        protected override void InitItemProperties(Item item)
+        {
+            base.InitItemProperties(item);
+
+            ApiKey = Sitecore.StringUtil.GetString(item.Fields["Api Key"]);
+            ErrorMessage = Sitecore.StringUtil.GetString(item.Fields["Error Message"]);
+        }
+
+        protected override void UpdateItemFields(Item item)
+        {
+            base.UpdateItemFields(item);
+
+            item.Fields["Api Key"].SetValue(ApiKey, true);
+            item.Fields["Error Message"].SetValue(ErrorMessage, true);
+        }
+    }
+}
+
+//Site Key: 6LeitsoqAAAAAOlZsRAAN_nc5oLKFHybfvzxx5RH
+//Secret Key: 6LeitsoqAAAAABD2SzIRqB-jgoh-vn6iYl_ANXyp
